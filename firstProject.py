51a3f3e7b8117983ae6575bfe7b109c1a44f2465
JustAnotherDevFromLA <70996442+JustAnotherDevFromLA@users.noreply.github.com>
2020-09-08 22:02:35 -0700

    Add files via upload

---
+++ firstProject.py
@@ -0,0 +1,17 @@
+
+
+user_name = "Artashes Kocharyan"
+
+user_hours = 0
+
+print("Programmer: " + user_name)
+print("Hours Worked: ", user_hours)
+
+if user_hours <= 100:
+    print("This guy is a fucking loser noob")
+elif user_hours <= 200:
+    print("This guy will never get a job")
+elif user_hours <= 500:
+    print("better but keep going!")
+else:
+    print("LETS GOOOO")
