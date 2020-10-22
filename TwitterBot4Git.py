1199069adb9685f66b2517e5aa23ad378e2b2efc
Artashes A Kocharyan <70996442+JustAnotherDevFromLA@users.noreply.github.com>
2020-10-21 22:13:02 -0700

    Update TwitterBot4Git.py
    Added documentation

--- TwitterBot4Git.py
+++ TwitterBot4Git.py
@@ -8,45 +8,45 @@ import os
 import json
 from tweepy import OAuthHandler
 
+#placeholders for the keys which are needed to interact with twitter API
 CONSUMER_KEY = ''
 CONSUMER_SECRET = '' 
 ACCESS_KEY = ''
 ACCESS_SECRET = ''
 
-
+#autharize the access with twitter 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET) 
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 logger = logging.getLogger()
-target = "@" 
 
-class FavRetweetListener(tweepy.StreamListener):
+class FavRetweetListener(tweepy.StreamListener):    #used to filter input stream from twitter
     def __init__(self, api):
         self.api = api
         self.me = api.me()
         
-    def on_status(self, tweet):
+    def on_status(self, tweet):         
         logger.info(f"Processing tweet id {tweet.id}")
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
             return
-        if not tweet.favorited:
+        if not tweet.favorited: #check if the tweet isnt favorited and then favorite it 
             try:
                 tweet.favorite()
             except Exception as e:
-                logger.error("Error on fav", exc_info=True)
-        if not tweet.retweeted:
+                logger.error("Error on fav", exc_info=True)  #try-catch with error handling with logger
+        if not tweet.retweeted:  #check if the tweet isnt favorited and then favorite it 
             try:
                 tweet.retweet()
             except Exception as e:
-               logger.error("Error on fav and retweet", exc_info=True)
+               logger.error("Error on fav and retweet", exc_info=True)  #try-catch with error handling with logger
     
-    def on_exception(self, exception):
+    def on_exception(self, exception):  #used for handling autodisconnect from twitter servers 
         print(exception)
         return
 
-    def on_error(self, status):
+    def on_error(self, status): #used for error logging 
         logger.error(status)
 
 def retweetLiker(keywords):
@@ -56,13 +56,12 @@ def retweetLiker(keywords):
 
     
     
-while True:
-    print("Tweeting...")
+while True: 
+    print("Tweeting...")    #make sure bot is running
     try: 
-        print("Retweeting and Fav Posts Now")
-        retweetLiker(["YourHashtags"]) 
-       
+        print("Retweeting and Fav Posts Now")   
+        retweetLiker(["YourHashtags"]) #call function and intput desired hashtags for filtering
         break
     
-    except tweepy.TweepError:
+    except tweepy.TweepError:   #try catch to insure bot keeps running
         print("Error")     
